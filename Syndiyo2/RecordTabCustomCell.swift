7b2efdb887c77b1fff8d1f32eed349edcceffcf9
Alan Yu <alan3467@gmail.com>
2016-07-19 00:01:51 +0200

    added the beginning of a xib for the record tab

---
+++ Syndiyo2/RecordTabCustomCell.swift
@@ -0,0 +1,24 @@
+//
+//  RecordTabCustomCell.swift
+//  Syndiyo2
+//
+//  Created by Alan Yu on 7/19/16.
+//  Copyright © 2016 Alan-Yu. All rights reserved.
+//
+
+import UIKit
+
+class RecordTabCustomCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
