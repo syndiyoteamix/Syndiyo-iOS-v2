5941e83cb9df741d0d85b9cad36da7c62d530189
Alan Yu <alan3467@gmail.com>
2016-07-19 02:15:26 +0200

    Added a records and a email capability

---
+++ CustomRequestVC.swift
@@ -0,0 +1,78 @@
+//
+//  CustomRequestVC.swift
+//  Syndiyo2
+//
+//  Created by Alan Yu on 7/19/16.
+//  Copyright © 2016 Alan-Yu. All rights reserved.
+//
+
+import UIKit
+import MessageUI
+
+class CustomRequestVC: UIViewController,MFMailComposeViewControllerDelegate {
+    
+    
+    @IBOutlet weak var doctorEmailTextField: UITextField!
+    
+    
+    @IBOutlet weak var subjectTextField: UITextField!
+    
+    @IBOutlet weak var textView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func SendButtonTapped(sender: AnyObject) {
+        let mailComposeViewController = configuredMailComposeViewController()
+        if MFMailComposeViewController.canSendMail() {
+            self.presentViewController(mailComposeViewController, animated: true, completion: nil)
+        } else {
+            self.showSendMailErrorAlert()
+        }
+    }
+    
+    
+    
+    func configuredMailComposeViewController() -> MFMailComposeViewController {
+        let mailComposerVC = MFMailComposeViewController()
+        mailComposerVC.mailComposeDelegate = self
+        
+        
+        //        mailComposerVC.setToRecipients([doctorEmailTextField.text!])
+        //        mailComposerVC.setSubject(subjectTextField.text!)
+        //        mailComposerVC.setMessageBody(textView.text!, isHTML: false)
+        //
+        return mailComposerVC
+    }
+    
+    
+    func showSendMailErrorAlert() {
+        // update code here later
+        let sendMailErrorAlert = UIAlertView(title: "Error", message: "Could not send email, please check email configuration", delegate: self, cancelButtonTitle: "Ok")
+        sendMailErrorAlert.show()
+    }
+    
+    func mailComposeController(controller: MFMailComposeViewController, didFinishWithResult result: MFMailComposeResult, error: NSError?) {
+        controller.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+    
+}
