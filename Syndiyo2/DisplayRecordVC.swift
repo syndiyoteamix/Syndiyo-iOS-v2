c510cf119cd9d61934540f79480ed70545106259
Alan Yu <alan3467@gmail.com>
2016-07-19 22:51:09 +0200

    Safety commit

---
+++ Syndiyo2/DisplayRecordVC.swift
@@ -0,0 +1,46 @@
+//
+//  DisplayRecordVC.swift
+//  Syndiyo2
+//
+//  Created by Alan Yu on 7/19/16.
+//  Copyright © 2016 Alan-Yu. All rights reserved.
+//
+
+import UIKit
+
+class DisplayRecordVC: UIViewController {
+    
+    
+    
+    
+    
+    
+    
+    var name: String?
+    var description: String?
+    var date: NSDate?
+    var image: UIImage?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
